16e617175fb74cfedae40640522ae6809f888578
Taras Shevchuk <t.shevchuk@ifit.com>
2021-03-12 16:36:09 +0200

    Fixed TestUtils library

--- ExoPlayer.Testutils/Additions/FakeMediaSource.cs
+++ ExoPlayer.Testutils/Additions/FakeMediaSource.cs
@@ -1,26 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using Android.Runtime;
-using Java.Interop;
+﻿using Android.Runtime;
 
 namespace Com.Google.Android.Exoplayer2.Testutil
-{/*
-    public partial class FakeMediaSource : global::Com.Google.Android.Exoplayer2.Source.BaseMediaSource
+{
+    public partial class FakeMediaSource
     {
-        // Metadata.xml XPath method reference: path="/api/package[@name='com.google.android.exoplayer2.source.ads']/class[@name='AdsMediaSource']/method[@name='releasePeriod' and count(parameter)=1 and parameter[1][@type='com.google.android.exoplayer2.source.MediaPeriod']]"
-        [Register("releasePeriod", "(Lcom/google/android/exoplayer2/source/MediaPeriod;)V", "")]
-        public override unsafe void ReleasePeriod(global::Com.Google.Android.Exoplayer2.Source.IMediaPeriod mediaPeriod)
-        {
-            const string __id = "releasePeriod.(Lcom/google/android/exoplayer2/source/MediaPeriod;)V";
-            try
-            {
-                JniArgumentValue* __args = stackalloc JniArgumentValue[1];
-                __args[0] = new JniArgumentValue((mediaPeriod == null) ? IntPtr.Zero : ((global::Java.Lang.Object)mediaPeriod).Handle);
-                _members.InstanceMethods.InvokeAbstractVoidMethod(__id, this, __args);
-            }
-            finally
-            {
-            }
-        }
-    }*/
+		// Metadata.xml XPath method reference: path="/api/package[@name='com.google.android.exoplayer2.testutil']/class[@name='FakeMediaSource']/method[@name='getInitialTimeline' and count(parameter)=0]"
+		[Register("getInitialTimeline", "()Lcom/google/android/exoplayer2/Timeline;", "GetGetInitialTimelineHandler")]
+		public unsafe global::Com.Google.Android.Exoplayer2.Timeline? GetInitialTimeline()
+		{
+			const string __id = "getInitialTimeline.()Lcom/google/android/exoplayer2/Timeline;";
+			try
+			{
+				var __rm = _members.InstanceMethods.InvokeVirtualObjectMethod(__id, this, null);
+				return global::Java.Lang.Object.GetObject<global::Com.Google.Android.Exoplayer2.Timeline>(__rm.Handle, JniHandleOwnership.TransferLocalRef);
+			}
+			finally
+			{
+			}
+		}
+	}
 }
