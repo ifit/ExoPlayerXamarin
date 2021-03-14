bfd60d352c33e5afe3af62ece579fd62b39cea61
Taras Shevchuk <t.shevchuk@ifit.com>
2021-03-15 01:32:39 +0200

    Core & Common fixes

---
+++ ExoPlayer.Core/Additions/IExoPlayer.cs
@@ -0,0 +1,41 @@
+﻿namespace Com.Google.Android.Exoplayer2
+{
+    public partial interface IExoPlayer
+    {
+        bool PlayWhenReady
+        {
+            get => GetPlayWhenReady ();
+            set => SetPlayWhenReady (value);
+        }
+
+        int RepeatMode
+        {
+            get => GetRepeatMode ();
+            set => SetRepeatMode (value);
+        }
+
+        bool ShuffleModeEnabled
+        {
+            get => GetShuffleModeEnabled ();
+            set => SetShuffleModeEnabled (value);
+        }
+    }
+
+    internal partial class IExoPlayerInvoker
+    {
+        public bool GetPlayWhenReady()
+        {
+            return PlayWhenReady;
+        }
+
+        public int GetRepeatMode()
+        {
+            return RepeatMode;
+        }
+
+        public bool GetShuffleModeEnabled()
+        {
+            return ShuffleModeEnabled;
+        }
+    }
+}
